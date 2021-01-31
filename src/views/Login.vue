f9dc2465086b0aeaaf4aed077c77ec2670260025
Tanyiqu <1953649096@qq.com>
2021-01-31 20:13:38 +0800

    删除不需要的项目文件

---
+++ src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div>111</div>
+    <div>111</div>
+    <div>111</div>
+    <div>111</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  components: {},
+};
+</script>
+
+<style lang="less" scoped>
+</style>
