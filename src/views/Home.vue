ccad5d0e5b3eeeee54392808dfaed1f20e991a29
Tanyiqu <1953649096@qq.com>
2021-02-01 11:59:47 +0800

    首页 - 引入布局容器

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>home</div>
+  <el-container class="container">
+    <el-header class="header">Header</el-header>
+    <el-container>
+      <el-aside class="aside" width="230px">Aside</el-aside>
+      <el-main class="main">Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -9,4 +15,17 @@ export default {
 </script>
 
 <style lang="less">
+.container {
+  height: 100%;
+  .header {
+    line-height: 60px;
+    background-color: #b3c0d1;
+  }
+  .aside {
+    background-color: #d3dce6;
+  }
+  .main {
+    background-color: #e9eef3;
+  }
+}
 </style>
