5669ede0d1c446c151e0da7b6adfbd24d6405898
Tanyiqu <1953649096@qq.com>
2021-02-01 13:09:56 +0800

    首页 - 头部和侧边

---
+++ src/components/Home/home-header.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-row>
+    <el-col :span="4"> LOGO </el-col>
+    <el-col class="title-div" :span="19">
+      <h2>电商后台管理系统</h2>
+    </el-col>
+    <el-col class="logout-div" :span="1">
+      <a class="logout" href="">退出</a>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,74 @@
 <template>
   <el-container class="container">
-    <el-header class="header">Header</el-header>
+    <!-- header -->
+    <el-header class="header">
+      <Header />
+    </el-header>
+    <!-- container -->
     <el-container>
-      <el-aside class="aside" width="230px">Aside</el-aside>
-      <el-main class="main">Main</el-main>
+      <!-- Menu -->
+      <el-aside class="aside" width="230px">
+        <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+          @open="handleOpen"
+          @close="handleClose"
+        >
+          <!-- 1 -->
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-menu"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item-group>
+              <template slot="title">管理你的用户</template>
+              <el-menu-item index="1-1">
+                <span>用户列表</span>
+              </el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+
+          <!-- 2 -->
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-menu"></i>
+              <span>商品管理</span>
+            </template>
+            <el-menu-item-group>
+              <template slot="title">管理你的商品</template>
+              <el-menu-item index="2-1">商品列表</el-menu-item>
+              <el-menu-item index="2-2">分类参数</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+
+          <!-- 3 -->
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-menu"></i>
+              <span>订单管理</span>
+            </template>
+            <el-menu-item-group>
+              <template slot="title">管理你的订单</template>
+              <el-menu-item index="3-1">订单列表</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+
+      <!-- Main -->
+      <el-main class="main">
+          
+      </el-main>
     </el-container>
   </el-container>
 </template>
 
 <script>
+import Header from '@/components/Home/home-header.vue';
 export default {
-
+  components: {
+    'Header': Header
+  }
 }
 </script>
 
@@ -20,6 +78,12 @@ export default {
   .header {
     line-height: 60px;
     background-color: #b3c0d1;
+    .title-div {
+      text-align: center;
+    }
+    .logout-div {
+      text-align: center;
+    }
   }
   .aside {
     background-color: #d3dce6;
