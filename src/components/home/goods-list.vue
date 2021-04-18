c915eab47d8d8a54fa68693c0ba442b5b09b711c
Tanyiqu <1953649096@qq.com>
2021-04-18 16:39:11 +0800

    基本完成

--- src/components/home/goods-list.vue
+++ src/components/home/goods-list.vue
@@ -1,12 +1,118 @@
 <template>
-  <div>商品列表</div>
+  <div>
+    <el-table :data="tableData" max-height="840px">
+      <el-table-column label="商品编号" prop="id"> </el-table-column>
+
+      <el-table-column label="名称" prop="name"> </el-table-column>
+
+      <el-table-column label="定价" prop="price"> </el-table-column>
+
+      <el-table-column label="现价" prop="present_price"> </el-table-column>
+
+      <el-table-column label="库存" prop="inventory"> </el-table-column>
+
+      <el-table-column label="销量" prop="sales"> </el-table-column>
+
+      <el-table-column label="订单状态" prop="status"> </el-table-column>
+
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            type="danger"
+            @click="handleDelete(scope.$index, scope.row)"
+          >
+            删除
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <el-pagination
+      class="pagination"
+      background
+      layout="prev, pager, next"
+      :total="50"
+      :page-size="12"
+    >
+    </el-pagination>
+  </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      tableData: [{
+        id: '10001',
+        name: '一加OnePlus 9Pro系列手机',
+        price: '3799.00',
+        present_price: '3799.00',
+        inventory: 9988,
+        sales: 11,
+        status: '正常'
+      }, {
+        id: '10001',
+        name: '一加OnePlus 9Pro系列手机',
+        price: '3799.00',
+        present_price: '3799.00',
+        inventory: 9988,
+        sales: 11,
+        status: '正常'
+      }, {
+        id: '10001',
+        name: '一加OnePlus 9Pro系列手机',
+        price: '3799.00',
+        present_price: '3799.00',
+        inventory: 9988,
+        sales: 11,
+        status: '正常'
+      }, {
+        id: '10001',
+        name: '一加OnePlus 9Pro系列手机',
+        price: '3799.00',
+        present_price: '3799.00',
+        inventory: 9988,
+        sales: 11,
+        status: '正常'
+      }, {
+        id: '10001',
+        name: '一加OnePlus 9Pro系列手机',
+        price: '3799.00',
+        present_price: '3799.00',
+        inventory: 9988,
+        sales: 11,
+        status: '正常'
+      }, {
+        id: '10001',
+        name: '一加OnePlus 9Pro系列手机',
+        price: '3799.00',
+        present_price: '3799.00',
+        inventory: 9988,
+        sales: 11,
+        status: '正常'
+      },]
+    }
+  },
 
+  methods: {
+    handleDelete(index, row) {
+      this.$alert(`删除 “${row.id} ${row.name}” ？`, '确认删除', {
+        confirmButtonText: '确定',
+        callback: action => {
+          if (action === 'cancel') {
+            this.$message.info('操作取消');
+          }
+        }
+      });
+    }
+  },
 }
 </script>
 
 <style lang="less" scoped>
+.pagination {
+  margin-top: 10px;
+  margin-bottom: 12px;
+}
 </style>
--- src/components/home/order-list.vue
+++ src/components/home/order-list.vue
@@ -1,12 +1,111 @@
 <template>
-  <div>订单列表</div>
+  <div>
+    <el-table :data="tableData" max-height="840px">
+      <el-table-column label="编号" prop="id"> </el-table-column>
+
+      <el-table-column label="制作商" prop="producer"> </el-table-column>
+
+      <el-table-column label="商品总额" prop="total_amount"> </el-table-column>
+
+      <el-table-column label="应付总额" prop="payment_amount">
+      </el-table-column>
+
+      <el-table-column label="订购时间" prop="date"> </el-table-column>
+
+      <el-table-column label="订单状态" prop="status"> </el-table-column>
+
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            type="danger"
+            @click="handleDelete(scope.$index, scope.row)"
+          >
+            删除
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <el-pagination
+      class="pagination"
+      background
+      layout="prev, pager, next"
+      :total="50"
+      :page-size="12"
+    >
+    </el-pagination>
+  </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      tableData: [{
+        id: 'EH115565478657',
+        producer: '一曲',
+        total_amount: '58.00',
+        payment_amount: '58.00',
+        date: '2021/4/18 16:22:01',
+        status: '交易成功'
+      }, {
+        id: 'EH115565478657',
+        producer: '一曲',
+        total_amount: '58.00',
+        payment_amount: '58.00',
+        date: '2021/4/18 16:22:01',
+        status: '交易成功'
+      }, {
+        id: 'EH115565478657',
+        producer: '一曲',
+        total_amount: '58.00',
+        payment_amount: '58.00',
+        date: '2021/4/18 16:22:01',
+        status: '交易成功'
+      }, {
+        id: 'EH115565478657',
+        producer: '一曲',
+        total_amount: '58.00',
+        payment_amount: '58.00',
+        date: '2021/4/18 16:22:01',
+        status: '交易成功'
+      }, {
+        id: 'EH115565478657',
+        producer: '一曲',
+        total_amount: '58.00',
+        payment_amount: '58.00',
+        date: '2021/4/18 16:22:01',
+        status: '交易成功'
+      }, {
+        id: 'EH115565478657',
+        producer: '一曲',
+        total_amount: '58.00',
+        payment_amount: '58.00',
+        date: '2021/4/18 16:22:01',
+        status: '交易成功'
+      },],
+    }
+  },
 
+  methods: {
+    handleDelete(index, row) {
+      this.$alert(`删除 “${row.id}” ？`, '确认删除', {
+        confirmButtonText: '确定',
+        callback: action => {
+          if (action === 'cancel') {
+            this.$message.info('操作取消');
+          }
+        }
+      });
+    }
+  },
 }
 </script>
 
 <style lang="less" scoped>
+.pagination {
+  margin-top: 10px;
+  margin-bottom: 12px;
+}
 </style>
